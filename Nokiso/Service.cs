c8d69d9d4c026f9f8db5be07e90257eb0276fe54
Soulier Antoine <soulierantoine@pc25.home>
2016-02-07 22:26:01 +0100

    Added convenient constructor and changed structure in Service class : added nested class TokenManager instead of having own file

--- Nokiso/Service.cs
+++ Nokiso/Service.cs
@@ -14,7 +14,10 @@ namespace Nokiso
 {
 	public class Service
 	{
-		public const string SERVER_URL = "http://xmazon.appspaces.fr";
+		private const string SERVER_URL = "http://xmazon.appspaces.fr";
+		private static int callsNumber = 0;
+		private TokenManager TkMgr;
+		private bool firstCall = true;
 
 		// operation that'll be executed on the server
 		public string Operation {
@@ -35,7 +38,7 @@ namespace Nokiso
 		}
 
 		// parameters in the body of the request
-		public HttpContent Body {
+		public Dictionary<string, string> Body {
 			get;
 			set;
 		}
@@ -45,75 +48,196 @@ namespace Nokiso
 			get;
 			set;
 		}
+			
 
-		// constructor
-		public Service (string operation, string method, string contentType, Dictionary<string, string> body, Dictionary<string, string> headers)
+		public Service()
 		{
-			Operation = operation;
-			Method = method;
-			ContentType = contentType;
-			Body = new FormUrlEncodedContent (body);
-			Headers = headers;
+			this.Method = "GET";
+			this.ContentType = "application/x-www-form-urlencoded";
+			this.Headers = new Dictionary<string, string> ();
+			this.Body = new Dictionary<string, string> ();
 		}
 
+		// for operation with get method
+		public Service (string operation) : this()
+		{
+			this.Operation = operation;
+		}
+			
+		// constructor used when initializing a TokenManager instance
+		public Service (string operation, string method, Dictionary<string, string> body) : this()
+		{
+			this.Operation = operation;
+			this.Method = method;
+			this.Body = body;
+		}
+
+		// for operation with get method, body for specific use of the operation
+		public Service (string operation, Dictionary<string, string> body) : this()
+		{
+			this.Operation = operation;
+			this.Body = body;
+		}
+
+		// custom call
+		public Service (string operation, string method, string contentType, Dictionary<string, string> headers, Dictionary<string, string> body)
+		{
+			this.Operation = operation;
+			this.Method = method;
+			this.ContentType = contentType;
+			this.Body = body;
+			this.Headers = headers;
+		}
+			
 
 		public async Task<JsonValue> CallAsync()
 		{
 			using (HttpClient request = new HttpClient ())
 			{
-				request.BaseAddress = new Uri (SERVER_URL);
+				request.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue (this.ContentType));
 
-				request.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue (ContentType));
-				foreach (var param in Headers) {
-					request.DefaultRequestHeaders.Add (param.Key, param.Value);	
+				// get a token only if the operation is /oauth/token
+				// otherwise we'd get an infinite loop
+				if (firstCall && this.Operation != "/oauth/token") {
+					TkMgr = new TokenManager();
+					Task<JsonValue> resultGetAppToken = TkMgr.GetAppToken ();
+					JsonValue getTokenData = await resultGetAppToken;
+					string accessToken = getTokenData ["access_token"];
+
+					this.Headers.Add("Authorization", "Bearer " + accessToken);
+					this.firstCall = false;
+				}
+					
+				if (this.Headers.Count > 0) {
+					foreach (var param in this.Headers) {
+						request.DefaultRequestHeaders.Add (param.Key, param.Value);	
+					}
 				}
 
-				HttpResponseMessage response;
-				switch (Method) {
-				case "GET":
-					response = await request.GetAsync (Operation);
-					break;
-				case "POST":
-					response = await request.PostAsync (Operation, Body);
-					break;
-				case "PUT":
-					response = await request.PutAsync (Operation, Body);
-					break;
-				case "DELETE":
-					response = await request.DeleteAsync (Operation);
-					break;
-				default:
-					response = null;
-					break;
+				request.BaseAddress = new Uri (SERVER_URL);
+				HttpContent body = new FormUrlEncodedContent (this.Body);
+
+				HttpResponseMessage response = null;
+				if (this.Method != null && this.Operation != null) {
+					switch (this.Method) {
+					case "GET":
+						response = await request.GetAsync (this.Operation);
+						break;
+					case "POST":
+						response = await request.PostAsync (this.Operation, body);
+						break;
+					case "PUT":
+						response = await request.PutAsync (this.Operation, body);
+						break;
+					case "DELETE":
+						response = await request.DeleteAsync (this.Operation);
+						break;
+					default:
+						response = null;
+						break;
+					}
 				}
 	
-				if (response != null) {				
+				if (response != null) {
 					using (Stream responseStream = await response.Content.ReadAsStreamAsync ()) {
 						JsonValue data = await Task.Run (() => JsonObject.Load (responseStream));
 						responseStream.Close ();
 
+						// if the operation is to get or refresh a token, we can return data
+						// no "code" key in the response of "/oauth/token" operations
+						if (response.IsSuccessStatusCode && this.Operation == "/oauth/token") {
+							return data;
+						}
+						
+						int responseCode = data["code"];
+						if (responseCode != 0) {
+							TkMgr = new TokenManager();
+							Task<JsonValue> resultRefreshAppToken = TkMgr.RefreshAppToken ();
+							JsonValue refreshData = await resultRefreshAppToken;
+
+							if (refreshData != null) {
+								if (!refreshData.ContainsKey("access_token")) {
+									return null;
+								} else {
+									string newToken = refreshData ["access_token"];
+									this.Headers ["Authorization"] = "Bearer " + newToken;
+									await this.CallAsync ();
+								}
+							} else {
+								return null;
+							}
+						}
+
 						return data;
 					}
-				} else {
-					return null;
 				}
+
+				return null;
 			}
 		}
 
-
-		/* private string formatUrl(Dictionary<string, string> CallParams)
+		class TokenManager
 		{
-			if (CallParams == null)
-				return "";
+			private Service Service;
+			private const string Method = "POST";
+			private const string Operation = "/oauth/token";
+			private const string ContentType = "application/x-www-form-urlencoded";
+			private string refreshAppToken;
+			private string refreshClientToken;
 
-			List<string> listParams = new List<string>();
-
-			foreach (var param in CallParams) {
-				listParams.Add(param.Key + "=" + param.Value);
+			// get is public, in case we need to get the grant_type
+			public Dictionary<string,string> Body {
+				get;
+				private set;
 			}
 
-			return "?" + string.Join("&", listParams);
-		} */
+			public TokenManager()
+			{
+				this.Body = new Dictionary<string, string>();
+
+				this.Body.Add("grant_type", "client_credentials");
+				this.Body.Add("client_id", "8a1d8939-7ded-4e0c-9cb1-a27748edad62");
+				this.Body.Add("client_secret", "cdf2662153b94b1cef93a7513276256908fe8992");
+				this.Body.Add("refresh_token", "");
+				this.Body.Add("username", "");
+				this.Body.Add("password", "");
+
+				this.Service = new Service(Operation, Method, Body);
+			}
+
+			public async Task<JsonValue> GetAppToken()
+			{
+				this.Body ["grant_type"] = "client_credentials";
+				Task<JsonValue> result = this.Service.CallAsync ();
+				JsonValue data = await result;
+
+				// CallAsync already checked for successful answer
+				this.refreshAppToken = data ["refresh_token"];
+				return data;
+			}
+
+			public async Task<JsonValue> RefreshAppToken()
+			{
+				this.Body ["grant_type"] = "refresh_token";
+				this.Body ["refresh_token"] = refreshAppToken;
+				Task<JsonValue> result = this.Service.CallAsync ();
+				JsonValue data = await result;
+
+				this.refreshAppToken = data ["refresh_token"];
+				return data;
+			}
+
+			// grant_type : password
+			public void GetClientToken()
+			{
+				
+			}
+
+			// grant_type : refresh_token et mettre le refresh_token correspondant
+			public void RefreshClientToken()
+			{
+				
+			}
+		}
 	}
 }
-
--- Nokiso/StorePage.xaml.cs
+++ Nokiso/StorePage.xaml.cs
@@ -25,10 +25,10 @@ namespace Nokiso
 			int responseCode = data ["code"];
 
 			if (data != null) {
-				if (responseCode != 200 && responseCode != 0) {
+				if (responseCode != 0) {
 					if (responseCode != 401) {
-						Console.WriteLine ("Something went wrong with the request...");
-						this.Result.Text = "Something went wrong with the request...";
+						Console.WriteLine ("Something went wrong with the request");
+						this.Result.Text = "Something went wrong with the request";
 					} else {
 						Console.WriteLine ("Invalid token or refresh token");
 						this.Result.Text = "Invalid token or refresh token";
@@ -42,25 +42,15 @@ namespace Nokiso
 
 		private async void GetStore()
 		{
-			// string Method = "GET";
-			// string Operation = "/store/list";
-			string Method = "POST";
-			string Operation = "/oauth/token";
-			string ContentType = "application/x-www-form-urlencoded";
-
-			Dictionary<string, string> Body = new Dictionary<string, string> ();
-			Body.Add ("grant_type", "client_credentials");
-			Body.Add("client_id", "8a1d8939-7ded-4e0c-9cb1-a27748edad62");
-			Body.Add("client_secret", "cdf2662153b94b1cef93a7513276256908fe8992");
-
-			Dictionary<string, string> Headers = new Dictionary<string, string> ();
-			Headers.Add ("Authorization", "Bearer faa3e81a3d784725ff8a8893e600200317fd14ae");
-		
-			Service s = new Service (Operation, Method, ContentType, Body, Headers);
+			Service s = new Service ("/store/list");
 			Task<JsonValue> result = s.CallAsync();
 
 			JsonValue data = await result;
-			Console.WriteLine ("Result : {0}", data);
+			if (data != null) {
+				Console.WriteLine ("Result : {0}", data);
+			} else {
+				Console.WriteLine ("Something went wrong with the request");
+			}
 			// UpdateUI (data);
 		}
 	}
