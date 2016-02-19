98c0bdbe2c78ae59c3eff60d52783ecaf147ab53
NawakoDev <nawako_68@hotmail.fr>
2016-02-19 13:54:26 +0100

    Added registration option

--- Nokiso/SignUpPage.xaml.cs
+++ Nokiso/SignUpPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using System.Json;
+using System.Threading.Tasks;
 
 namespace Nokiso
 {
@@ -11,6 +13,57 @@ namespace Nokiso
 		{
 			this.Title = "Sign Up";
 			InitializeComponent ();
+		}
+
+		private async void OnRegisterButtonClicked (object sender, EventArgs args) {
+
+			if (AreCredentialsCorrect() && IsValidEmail(mailEntry.Text)) {
+				PostUser ();
+			} else {
+				passwordEntry.Text = string.Empty;
+			}
+		}
+
+		private async void PostUser ()
+		{
+			Dictionary<string,string> body = new Dictionary<string, string> ();
+			body.Add ("email", mailEntry.Text);
+			body.Add ("firstname", firstNameEntry.Text);
+			body.Add ("lastname", lastNameEntry.Text);
+			body.Add ("password", passwordEntry.Text);
+			Service s = new Service ("/auth/subscribe", body, "app");
+			Task<JsonValue> result = s.CallAsync();
+
+			JsonValue data = await result;
+
+			if (!data.ContainsKey("erreur")) {
+				Console.WriteLine ("Result : {0}", data);
+			} else {
+				Console.WriteLine ("Something went wrong with the request");
+			}
+		}
+
+		bool AreCredentialsCorrect ()
+		{
+			bool credits = false;
+			if (mailEntry.Text.Length > 4 &&
+				passwordEntry.Text.Length >= 6)
+				credits = true;
+			else
+				DisplayAlert ("Login / Password invalid", "Username should be superior to 4 characters, password should be superior to 6 characters", "OK");
+
+			return credits;
+		}
+
+		bool IsValidEmail(string email)
+		{
+			try {
+				var addr = new System.Net.Mail.MailAddress(email);
+				return addr.Address == email;
+			}
+			catch {
+				return false;
+			}
 		}
 	}
 }
